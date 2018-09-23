82408313f7cc0ce6097c48a590428edc1d24a028
Unknown <clarkkee1999@gmail.com>
2018-09-22 22:59:21 -0700

    Second challenge of section 5
    1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.
    2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
    3- Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
    4- Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, you can display the secret number on the console first.)
    5- Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.

---
+++ Exercises/Beginner/Section5Ex2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Section5Ex2
+{
+    class Program
+    {
+        /* 
+       
+        */
+        static void mission1()
+        {
+            int minValue = 1;
+            int maxValue = 100;
+            int count = 0;
+
+            for (int i = minValue; i <= maxValue; i++) 
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Total count divisable by 3 and with no remainder: " + count);
+        }
+
+        static void mission2()
+        {
+            var sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter a number or press ok to quit");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                {
+                    break;
+                }
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("All entered numbers added toger is: " + sum);
+        }
+
+        //static void mission3() //Get factorial of input number
+        //{
+        //    Console.WriteLine("Enter a number to find the factorial for");
+        //    int factorial = Convert.ToInt32(Console.ReadLine());
+
+        //    for (int i = 0; i < factorial; i++) 
+        //    {
+        //        factorial = factorial * i;
+        //    }
+        //    Console.WriteLine(factorial);
+        //}
+
+        static void mission4()
+        {
+
+            Random random = new Random();
+            int computerNumber = random.Next(1,11);
+            int guesses = 4;
+
+            Console.WriteLine("I have secected a number, if you gues it right, you get a piece of candy! you get 4 tries.");
+            Console.WriteLine("My number is between 1 and 10, enther a number to guess");
+
+            for (int i = 0; i < guesses; i++) 
+            {
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+                if (userGuess == computerNumber)
+                {
+                    Console.WriteLine("You Win!");
+                    goto end;
+                }
+                else
+                {
+                    Console.WriteLine("Nope! try, again. that was attempt {0}", i+1);
+                }
+            }
+            Console.WriteLine("You Loose!");
+            end:;
+        }
+
+        //static void mission5()
+        //{
+        //    //-Write a program and ask the user to enter a series of numbers separated by comma.Find the maximum of the numbers and display it on the console. For example,
+        //    //if the user enters “5, 3, 8, 1, 4", the program should display 8.
+        //    Console.Write("Enter commoa separated numbers: ");
+        //    var input = Console.ReadLine();
+
+
+
+        //}
+
+        static void Main(string[] args)
+        {
+            mission1();
+            mission2();
+            //mission3(); //Failed
+            mission4();
+            //mission5(); //Failed, dont understand
+            Console.ReadKey();
+        }
+    }
+}
